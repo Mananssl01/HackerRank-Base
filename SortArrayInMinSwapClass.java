9f54b38ebc00dca17279c71af930c416f37ecf80
Mananssl01 <manan.asnl@gmail.com>
2019-07-31 13:01:56 +0530

    Committing class for min swaps
    Committing class for min swaps

---
+++ SortArrayInMinSwapClass.java
@@ -0,0 +1,42 @@
+// Minimum swaps 2 - HR
+
+// given an unordered array containing n consecutive integers with no duplicate, find min. number os swaps to sort it . in one swap, only two elements can be swapped.
+// solution hint - since given numbers are consecutive without duplicates , the 'index' should contain the element 'index+1' in the sorted array.
+package com.test.hr;
+
+import java.util.function.Function;
+
+public class SortArrayInMinSwapClass {
+    public static void main(String[] args) {
+        int [] arr = {7, 1, 3, 2, 4, 5, 6};
+    System.out.println("result "+swapFunction.apply(arr));
+    }
+
+    public static Function<int[],Integer> swapFunction = arr ->
+    {
+        int count=0;int temp=-1;int limit=0;
+        for (int i = 0; true ; i++) {       // note that condition portion is true for infinite loop, which exits on break.
+            if(i!=arr[i]-1)
+            {
+                //swap elements
+                count++;
+                temp=arr[arr[i]-1];
+                arr[arr[i]-1]=arr[i];
+                arr[i]=temp;
+            }else
+                limit++;
+
+            if(limit==arr.length)
+                break;
+
+            if(i==arr.length-1)
+            {
+                i=-1;
+                limit=0;
+            }
+
+        }
+        return count;
+
+    };
+}
